f9a5ba0cd569eb15ccf0183f32b7458cda753ac5
Atiye.beigirad@gmail.com <atiye.beigirad@gmail.com>
2023-04-21 10:39:35 +0330

    add .editorconfig

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 const list = document.getElementsByClassName("list")[0];
 const newReminder = document.getElementById("newReminder");
 function createreminder(id, [message, Date, caption]) {
-  const li = document.createElement("li");
-  li.id = id;
-  li.className = "reminder";
-  const div = document.createElement("div");
-  div.className = "text";
-  div.innerText = message;
+    const li = document.createElement("li");
+    li.id = id;
+    li.className = "reminder";
+    const div = document.createElement("div");
+    div.className = "text";
+    div.innerText = message;
 
-  const div2 = document.createElement("div");
-  div2.className = "date";
-  div2.innerText = Date;
-  div.appendChild(div2);
+    const div2 = document.createElement("div");
+    div2.className = "date";
+    div2.innerText = Date;
+    div.appendChild(div2);
 
-  const div3 = document.createElement("div");
-  div3.className = "caption";
-  div3.innerText = caption;
-  div.appendChild(div3);
-  if (message && message.length > 45) {
-    alert("we only accept 45 charecters");
-    return;
-  } else if (!message) {
-    alert("please enter a reminder");
-    return;
-  }
-  const actioncontainer = document.createElement("div");
-  actioncontainer.className = "actions";
-
-  const Checkbtn = document.createElement("button");
-  Checkbtn.className = "btn-check";
-  Checkbtn.innerText = "Checked";
-
-  const Deletebtn = document.createElement("button");
-  Deletebtn.className = "btn-delete";
-  Deletebtn.innerText = "Delete";
-
-  Checkbtn.addEventListener("click", function () {
-    if (li.id == id) {
-      div.style.textDecoration = "line-through";
-      div.style.textDecorationColor = "red";
-      div.style.textEmphasis = "bold";
+    const div3 = document.createElement("div");
+    div3.className = "caption";
+    div3.innerText = caption;
+    div.appendChild(div3);
+    if (message && message.length > 45) {
+        alert("we only accept 45 charecters");
+        return;
+    } else if (!message) {
+        alert("please enter a reminder");
+        return;
     }
-  });
-  Deletebtn.addEventListener("click", function () {
-    if (li.id == id) {
-      list.removeChild(li);
-    }
-  });
+    const actioncontainer = document.createElement("div");
+    actioncontainer.className = "actions";
 
-  actioncontainer.appendChild(Checkbtn);
-  actioncontainer.appendChild(Deletebtn);
+    const Checkbtn = document.createElement("button");
+    Checkbtn.className = "btn-check";
+    Checkbtn.innerText = "Checked";
 
-  li.appendChild(div);
-  li.appendChild(actioncontainer);
-  return li;
+    const Deletebtn = document.createElement("button");
+    Deletebtn.className = "btn-delete";
+    Deletebtn.innerText = "Delete";
+
+    Checkbtn.addEventListener("click", function () {
+        if (li.id == id) {
+            div.style.textDecoration = "line-through";
+            div.style.textDecorationColor = "red";
+            div.style.textEmphasis = "bold";
+        }
+    });
+    Deletebtn.addEventListener("click", function () {
+        if (li.id == id) {
+            list.removeChild(li);
+        }
+    });
+
+    actioncontainer.appendChild(Checkbtn);
+    actioncontainer.appendChild(Deletebtn);
+
+    li.appendChild(div);
+    li.appendChild(actioncontainer);
+    return li;
 }
 newReminder.addEventListener("click", function () {
-  let message = prompt("please enter a reminder ");
-  let Date = prompt("please enter a date");
-  let caption = prompt("please write  the details");
-  let id = Math.ceil(Math.random() * 100);
-  let reminder = createreminder(id, [message, Date, caption]);
-  list.appendChild(reminder);
+    let message = prompt("please enter a reminder ");
+    let Date = prompt("please enter a date");
+    let caption = prompt("please write  the details");
+    let id = Math.ceil(Math.random() * 100);
+    let reminder = createreminder(id, [message, Date, caption]);
+    list.appendChild(reminder);
 });
